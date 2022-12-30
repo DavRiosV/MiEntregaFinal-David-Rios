1be2c8d086656a5b0df6f869a1bc5aa49da3fc80
David Rios <david.r.v@hotmail.com>
2022-12-29 23:37:35 -0300

    upgrade

--- js/carrito.js
+++ js/carrito.js
@@ -112,4 +112,20 @@ const btnswitch = document.querySelector(`#switch`)
 btnswitch.addEventListener('click', () =>{
     document.body.classList.toggle('dark');
     btnswitch.classList.toggle('active'); 
+    
+    // guardar la seleccion del modo oscuro en localStorage 
+    
+if(document.body.classList.contains('dark')){
+    localStorage.setItem('dark-mode', 'true');
+} else {
+    localStorage.setItem('dark-mode', 'false');
+}
 });
+
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnswitch.classList.add('active'); 
+} else {
+    document.body.classList.remove('dark');
+    btnswitch.classList.remove('active');
+}
--- js/main.js
+++ js/main.js
@@ -198,7 +198,7 @@ const productos = [
     },
     {
         id: "Peluche-14",
-        titulo: "Ditto Bulbasaur",
+        titulo: "Ditto Charmander",
         imagen: "./new-img/ditto-charmander.jpeg",
         categoria: {
             nombre: "Peluches",
@@ -476,4 +476,20 @@ const btnswitch = document.querySelector(`#switch`)
 btnswitch.addEventListener('click', () =>{
     document.body.classList.toggle('dark');
     btnswitch.classList.toggle('active'); 
+    
+    // guardar la seleccion del modo oscuro en localStorage 
+    
+if(document.body.classList.contains('dark')){
+    localStorage.setItem('dark-mode', 'true');
+} else {
+    localStorage.setItem('dark-mode', 'false');
+}
 });
+
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnswitch.classList.add('active'); 
+} else {
+    document.body.classList.remove('dark');
+    btnswitch.classList.remove('active');
+}
