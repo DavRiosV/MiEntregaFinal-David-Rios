2dcc51fe0233d5d0a3f6afe5acab898f335f5fdd
David Rios <david.r.v@hotmail.com>
2023-01-21 16:20:55 -0300

    correcion swiper

--- js/carrito.js
+++ js/carrito.js
@@ -1,3 +1,26 @@
+const btnswitch = document.querySelector(`#switch`)
+
+btnswitch.addEventListener('click', () =>{
+    document.body.classList.toggle('dark');
+    btnswitch.classList.toggle('active'); 
+    
+    // guardar la seleccion del modo oscuro en localStorage 
+    
+if(document.body.classList.contains('dark')){
+    localStorage.setItem('dark-mode', 'true');
+} else {
+    localStorage.setItem('dark-mode', 'false');
+}
+});
+
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnswitch.classList.add('active'); 
+} else {
+    document.body.classList.remove('dark');
+    btnswitch.classList.remove('active');
+}
+
 let enCarrito = localStorage.getItem("productos-en-carrito");
 enCarrito = JSON.parse(enCarrito);
 
